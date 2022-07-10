5cf7ee9951992006bd547fdacd32feeca3539561
Kristian Duda <kristianduda83@gmail.com>
2022-07-10 21:19:06 +0200

    .gitignore

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,42 +1,18 @@
-import { Injectable, Inject } from '@nestjs/common';
-import { Db } from 'mongodb';
+import { Injectable, Inject } from "@nestjs/common";
+import { Db } from "mongodb";
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[];
-
   constructor(
-    @Inject('DATABASE_CONNECTION')
-    private db: Db,
-
-  ) {
-    this.users = [
-      {
-        userId: 1,
-        username: 'john',
-        password: 'changeme',
-        permissions: ['profile']
-      },
-      {
-        userId: 2,
-        username: 'chris',
-        password: 'secret',
-        permissions: ['profile']
-      },
-      {
-        userId: 3,
-        username: 'maria',
-        password: 'guess',
-        permissions: ['profile']
-      },
-    ];
-  }
+    @Inject("DATABASE_CONNECTION")
+    private db: Db
+  ) {}
 
   async findOne(username: string): Promise<User | undefined> {
-    return await this.db.collection('users').findOne({
-      username
+    return await this.db.collection("users").findOne({
+      username,
     });
   }
 }
