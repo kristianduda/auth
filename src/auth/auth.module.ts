3c986026c78b6b9576c52e79374b3d578af01de4
Kristian Duda <kristianduda83@gmail.com>
2022-07-10 21:11:10 +0200

    ConfigService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
@@ -11,9 +11,14 @@ import { LocalStrategy } from './strategies/local.strategy';
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1h' },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("SECRET"),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
--- src/db.module.ts
+++ src/db.module.ts
@@ -1,20 +1,24 @@
-import { Module } from '@nestjs/common';
-import { MongoClient, Db } from 'mongodb';
+import { Module } from "@nestjs/common";
+import { MongoClient, Db } from "mongodb";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
   providers: [
     {
-      provide: 'DATABASE_CONNECTION',
-      useFactory: async (): Promise<Db> => {
+      provide: "DATABASE_CONNECTION",
+      useFactory: async (configService: ConfigService): Promise<Db> => {
         try {
-          const client = await MongoClient.connect('mongodb+srv://dev:RZh9XXFQruLzLLZv@cluster0.fufjo.mongodb.net');
-          return client.db('auth');
+          const client = await MongoClient.connect(
+            configService.get<string>("CONNECTION_STRING")
+          );
+          return client.db(configService.get<string>("DATABASE"));
         } catch (e) {
           throw e;
         }
-      }
+      },
+      inject: [ConfigService],
     },
   ],
-  exports: ['DATABASE_CONNECTION'],
+  exports: ["DATABASE_CONNECTION"],
 })
 export class DatabaseModule {}
